8508a20800703a3efec070a95bd0d7ace1426d4e
Tulio Parreiras <tulio.parreiras@bilue.com.au>
2022-04-03 21:49:33 +1000

    Merge tests

--- Dog Breeds App/Dog Breeds AppTests/Dog_Breeds_AppTests.swift
+++ Dog Breeds App/Dog Breeds AppTests/Dog_Breeds_AppTests.swift
@@ -65,27 +65,29 @@ class Dog_Breeds_AppTests: XCTestCase {
         XCTAssertEqual(numberOfRows, 10)
     }
     
-    func test_viewLoad_rendersBreedsNames() {
+    func test_viewLoad_rendersBreeds() {
         let sut = BreedListViewController()
         
         sut.loadViewIfNeeded()
         
         let dataSource = sut.tableView.dataSource
-        let names: Array = [
-            "Affenpinscher",
-            "Afghan Hound",
-            "African Hunting Dog",
-            "Airedale Terrier",
-            "Akbash Dog",
-            "Akita",
-            "Alapaha Blue Blood",
-            "Alaskan Husky",
-            "Alaskan Malamute",
-            "American Bulldog",
+        let dogBreeds: [DogBreed] = [
+            DogBreed(name: "Affenpinscher", temperament: "Stubborn, Curious, Playful, Adventurous, Active, Fun-loving"),
+            DogBreed(name: "Afghan Hound", temperament: "Aloof, Clownish, Dignified, Independent, Happy"),
+            DogBreed(name: "African Hunting Dog", temperament: "Wild, Hardworking, Dutiful"),
+            DogBreed(name: "Airedale Terrier", temperament: "Outgoing, Friendly, Alert, Confident, Intelligent, Courageous"),
+            DogBreed(name: "Akbash Dog", temperament: "Loyal, Independent, Intelligent, Brave"),
+            DogBreed(name: "Akita", temperament: "Docile, Alert, Responsive, Dignified, Composed, Friendly, Receptive, Faithful, Courageous"),
+            DogBreed(name: "Alapaha Blue Blood", temperament: "Loving, Protective, Trainable, Dutiful, Responsible"),
+            DogBreed(name: "Alaskan Husky", temperament: "Friendly, Energetic, Loyal, Gentle, Confident"),
+            DogBreed(name: "Alaskan Malamute", temperament: "Friendly, Affectionate, Devoted, Loyal, Dignified, Playful"),
+            DogBreed(name: "American Bulldog", temperament: "Friendly, Assertive, Energetic, Loyal, Gentle, Confident, Dominant"),
         ]
         
-        for index in 0 ..< names.count {
-            let name = names[index]
+        for index in 0 ..< dogBreeds.count {
+            let dogBreed = dogBreeds[index]
+            let name = dogBreed.name
+            let temperament = dogBreed.temperament
             let cell = dataSource?.tableView(sut.tableView,
                                              cellForRowAt: IndexPath(row: index,
                                                                      section: 0))
@@ -94,35 +96,7 @@ class Dog_Breeds_AppTests: XCTestCase {
                 return
             }
             XCTAssertEqual(config.text, name)
-        }
-    }
-    
-    func test_viewLoad_rendersBreedsTemperaments() {
-        let sut = BreedListViewController()
-        
-        sut.loadViewIfNeeded()
-        
-        let dataSource = sut.tableView.dataSource
-        let temperaments: Array = [
-            "Stubborn, Curious, Playful, Adventurous, Active, Fun-loving",
-            "Aloof, Clownish, Dignified, Independent, Happy",
-            "Wild, Hardworking, Dutiful",
-            "Outgoing, Friendly, Alert, Confident, Intelligent, Courageous",
-            "Loyal, Independent, Intelligent, Brave",
-            "Docile, Alert, Responsive, Dignified, Composed, Friendly, Receptive, Faithful, Courageous",
-            "Loving, Protective, Trainable, Dutiful, Responsible",
-            "Friendly, Energetic, Loyal, Gentle, Confident",
-            "Friendly, Affectionate, Devoted, Loyal, Dignified, Playful",
-            "Friendly, Assertive, Energetic, Loyal, Gentle, Confident, Dominant",
-        ]
-        
-        for index in 0 ..< temperaments.count {
-            let temperament = temperaments[index]
-            let cell = dataSource?.tableView(sut.tableView,
-                                             cellForRowAt: IndexPath(row: index,
-                                                                     section: 0))
-            let config = cell?.contentConfiguration as? UIListContentConfiguration
-            XCTAssertEqual(config?.secondaryText, temperament)
+            XCTAssertEqual(config.secondaryText, temperament)
         }
     }
     
