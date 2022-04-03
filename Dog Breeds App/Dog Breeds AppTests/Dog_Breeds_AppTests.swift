3f7d1908a6ae600640bacf53113d13e067cbdbf2
Tulio Parreiras <tulio.parreiras@bilue.com.au>
2022-04-03 21:27:03 +1000

    BreedListViewController renders breeds names

--- Dog Breeds App/Dog Breeds AppTests/Dog_Breeds_AppTests.swift
+++ Dog Breeds App/Dog Breeds AppTests/Dog_Breeds_AppTests.swift
@@ -8,10 +8,32 @@
 import XCTest
 @testable import Dog_Breeds_App
 
-class BreedListViewController: UITableViewController {    
+class BreedListViewController: UITableViewController {
+    
+    let names: Array = [
+        "Affenpinscher",
+        "Afghan Hound",
+        "African Hunting Dog",
+        "Airedale Terrier",
+        "Akbash Dog",
+        "Akita",
+        "Alapaha Blue Blood",
+        "Alaskan Husky",
+        "Alaskan Malamute",
+        "American Bulldog",
+    ]
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let cell = UITableViewCell()
+        var config = cell.defaultContentConfiguration()
+        config.text = names[row]
+        cell.contentConfiguration = config
+        return cell
     }
     
 }
@@ -27,6 +49,35 @@ class Dog_Breeds_AppTests: XCTestCase {
         let numberOfRows = dataSource?.tableView(sut.tableView,
                                                  numberOfRowsInSection: 0)
         XCTAssertEqual(numberOfRows, 10)
+    }
+    
+    func test_viewLoad_rendersBreedsNames() {
+        let sut = BreedListViewController()
+        
+        sut.loadViewIfNeeded()
+        
+        let dataSource = sut.tableView.dataSource
+        let names: Array = [
+            "Affenpinscher",
+            "Afghan Hound",
+            "African Hunting Dog",
+            "Airedale Terrier",
+            "Akbash Dog",
+            "Akita",
+            "Alapaha Blue Blood",
+            "Alaskan Husky",
+            "Alaskan Malamute",
+            "American Bulldog",
+        ]
+        
+        for index in 0 ..< names.count {
+            let name = names[index]
+            let cell = dataSource?.tableView(sut.tableView,
+                                             cellForRowAt: IndexPath(row: index,
+                                                                     section: 0))
+            let config = cell?.contentConfiguration as? UIListContentConfiguration
+            XCTAssertEqual(config?.text, name)
+        }
     }
     
 }
