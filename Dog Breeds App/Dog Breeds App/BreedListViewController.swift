958abacc65a6d17b59d1ec8b19cb914a20279191
Tulio Parreiras <tulio.parreiras@bilue.com.au>
2022-04-03 22:05:31 +1000

    Reuse cells

--- Dog Breeds App/Dog Breeds App/BreedListViewController.swift
+++ Dog Breeds App/Dog Breeds App/BreedListViewController.swift
@@ -20,7 +20,21 @@ class BreedListViewController: UITableViewController {
         DogBreed(name: "Alaskan Husky", temperament: "Friendly, Energetic, Loyal, Gentle, Confident"),
         DogBreed(name: "Alaskan Malamute", temperament: "Friendly, Affectionate, Devoted, Loyal, Dignified, Playful"),
         DogBreed(name: "American Bulldog", temperament: "Friendly, Assertive, Energetic, Loyal, Gentle, Confident, Dominant"),
+        DogBreed(name: "American Bully", temperament: "Strong Willed, Stubborn, Friendly, Clownish, Affectionate, Loyal, Obedient, Intelligent, Courageous"),
+        DogBreed(name: "American Eskimo Dog", temperament: "Friendly, Alert, Reserved, Intelligent, Protective"),
+        DogBreed(name: "American Eskimo Dog (Miniature)", temperament: "Friendly, Alert, Reserved, Intelligent, Protective"),
+        DogBreed(name: "American Foxhound", temperament: "Kind, Sweet-Tempered, Loyal, Independent, Intelligent, Loving"),
+        DogBreed(name: "American Pit Bull Terrier", temperament: "Strong Willed, Stubborn, Friendly, Clownish, Affectionate, Loyal, Obedient, Intelligent, Courageous"),
+        DogBreed(name: "American Staffordshire Terrier", temperament: "Tenacious, Friendly, Devoted, Loyal, Attentive, Courageous"),
+        DogBreed(name: "American Water Spaniel", temperament: "Friendly, Energetic, Obedient, Intelligent, Protective, Trainable"),
+        DogBreed(name: "Anatolian Shepherd Dog", temperament: "Steady, Bold, Independent, Confident, Intelligent, Proud"),
+        DogBreed(name: "Appenzeller Sennenhund", temperament: "Reliable, Fearless, Energetic, Lively, Self-assured")
     ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         dogBreeds.count
@@ -29,13 +43,15 @@ class BreedListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let row = indexPath.row
         let breed = dogBreeds[row]
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         var config = cell.defaultContentConfiguration()
         config.text = breed.name
         config.secondaryText = breed.temperament
         cell.contentConfiguration = config
         if row % 2 == 0 {
             cell.backgroundColor = UIColor.lightGray.withAlphaComponent(0.2)
+        } else {
+            cell.backgroundColor = nil
         }
         return cell
     }
