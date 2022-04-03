d3298f7bc5513d0e4f66fa5ebe1421dac0c33747
Tulio Parreiras <tulio.parreiras@bilue.com.au>
2022-04-03 21:46:38 +1000

    Replace strings array with custom DogBreed class

--- Dog Breeds App/Dog Breeds AppTests/Dog_Breeds_AppTests.swift
+++ Dog Breeds App/Dog Breeds AppTests/Dog_Breeds_AppTests.swift
@@ -8,32 +8,31 @@
 import XCTest
 @testable import Dog_Breeds_App
 
+class DogBreed {
+    
+    let name: String
+    let temperament: String
+    
+    init(name: String, temperament: String) {
+        self.name = name
+        self.temperament = temperament
+    }
+    
+}
+
 class BreedListViewController: UITableViewController {
     
-    let names: Array = [
-        "Affenpinscher",
-        "Afghan Hound",
-        "African Hunting Dog",
-        "Airedale Terrier",
-        "Akbash Dog",
-        "Akita",
-        "Alapaha Blue Blood",
-        "Alaskan Husky",
-        "Alaskan Malamute",
-        "American Bulldog",
-    ]
-    
-    let temperaments: Array = [
-        "Stubborn, Curious, Playful, Adventurous, Active, Fun-loving",
-        "Aloof, Clownish, Dignified, Independent, Happy",
-        "Wild, Hardworking, Dutiful",
-        "Outgoing, Friendly, Alert, Confident, Intelligent, Courageous",
-        "Loyal, Independent, Intelligent, Brave",
-        "Docile, Alert, Responsive, Dignified, Composed, Friendly, Receptive, Faithful, Courageous",
-        "Loving, Protective, Trainable, Dutiful, Responsible",
-        "Friendly, Energetic, Loyal, Gentle, Confident",
-        "Friendly, Affectionate, Devoted, Loyal, Dignified, Playful",
-        "Friendly, Assertive, Energetic, Loyal, Gentle, Confident, Dominant",
+    let dogBreeds: [DogBreed] = [
+        DogBreed(name: "Affenpinscher", temperament: "Stubborn, Curious, Playful, Adventurous, Active, Fun-loving"),
+        DogBreed(name: "Afghan Hound", temperament: "Aloof, Clownish, Dignified, Independent, Happy"),
+        DogBreed(name: "African Hunting Dog", temperament: "Wild, Hardworking, Dutiful"),
+        DogBreed(name: "Airedale Terrier", temperament: "Outgoing, Friendly, Alert, Confident, Intelligent, Courageous"),
+        DogBreed(name: "Akbash Dog", temperament: "Loyal, Independent, Intelligent, Brave"),
+        DogBreed(name: "Akita", temperament: "Docile, Alert, Responsive, Dignified, Composed, Friendly, Receptive, Faithful, Courageous"),
+        DogBreed(name: "Alapaha Blue Blood", temperament: "Loving, Protective, Trainable, Dutiful, Responsible"),
+        DogBreed(name: "Alaskan Husky", temperament: "Friendly, Energetic, Loyal, Gentle, Confident"),
+        DogBreed(name: "Alaskan Malamute", temperament: "Friendly, Affectionate, Devoted, Loyal, Dignified, Playful"),
+        DogBreed(name: "American Bulldog", temperament: "Friendly, Assertive, Energetic, Loyal, Gentle, Confident, Dominant"),
     ]
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -42,10 +41,11 @@ class BreedListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let row = indexPath.row
+        let breed = dogBreeds[row]
         let cell = UITableViewCell()
         var config = cell.defaultContentConfiguration()
-        config.text = names[row]
-        config.secondaryText = temperaments[row]
+        config.text = breed.name
+        config.secondaryText = breed.temperament
         cell.contentConfiguration = config
         return cell
     }
@@ -89,8 +89,11 @@ class Dog_Breeds_AppTests: XCTestCase {
             let cell = dataSource?.tableView(sut.tableView,
                                              cellForRowAt: IndexPath(row: index,
                                                                      section: 0))
-            let config = cell?.contentConfiguration as? UIListContentConfiguration
-            XCTAssertEqual(config?.text, name)
+            guard let config = cell?.contentConfiguration as? UIListContentConfiguration else {
+                XCTFail("Expected that cell configuration is \(UIListContentConfiguration.self), got \(String(describing: cell?.contentConfiguration)) instead")
+                return
+            }
+            XCTAssertEqual(config.text, name)
         }
     }
     
